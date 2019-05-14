c5662da580d88c070b336e6275ab9054a345f973
jw <jan.wildenhain@gmail.com>
2019-05-14 16:05:26 +0100

    update REST api

--- html/molclass/api/index.php
+++ html/molclass/api/index.php
@@ -22,6 +22,10 @@ $app->get('/dataset/:id',	'getDataset');
 $app->get('/dataset/:id/compounds',	'getDatasetCompounds');
 
 $app->get('/compound/:id',	'getCompound');
+$app->get('/compound/:id/structurefingerprint',	'getCompoundStructuralFingerprint');
+$app->get('/compound/:id/propertyfingerprint',	'getCompoundPropertyFingerprint');
+$app->get('/compound/:id/modelfingerprint',	'getCompoundModelFingerprint');
+
 $app->get('/compound/:id/models',	'getCompoundModels');
 
 $app->get('/model', 'getModels');
@@ -81,7 +85,7 @@ function getCompound($id) {
 
         $id = str_replace("_","/",$id);
 
-	$sql = "SELECT inchi_key.*, sdftags.* FROM sdftags left join inchi_key using (mol_id) WHERE mol_id=:id or inchi_key.smiles=:id or inchi_key.inchi=:id or inchi_key.inchi_key=:id";
+	$sql = "SELECT a.*, b.inchi_key, b.smiles, c.compound_name, c.class, c.classifier, c.activity_class, c.*, b.inchi FROM sdftags c left join inchi_key b using (mol_id) left join moldb_moldata a using (mol_id) WHERE ( compound_name=:id or mol_name=:id ) or mol_id=:id or ( b.smiles=:id or b.inchi=:id or b.inchi_key=:id )";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);  
@@ -96,11 +100,75 @@ function getCompound($id) {
 	}
 }
 
+function getCompoundStructuralFingerprint($id) {
+
+        $id = str_replace("_","/",$id);
+
+	$sql = "SELECT fingerprints.*, inchi_key.* FROM sdftags left join inchi_key using (mol_id) left join fingerprints using (mol_id) WHERE mol_id=:id or inchi_key.smiles=:id or inchi_key.inchi=:id or inchi_key.inchi_key=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$singledataset = $stmt->fetchObject(); # ->fetchAll(PDO::FETCH_OBJ);  
+		$db = null;
+		echo json_encode($singledataset); 
+		#echo '{"compounds": ' . json_encode($singledataset) . '}'; 
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function getCompoundPropertyFingerprint($id) {
+
+        $id = str_replace("_","/",$id);
+
+	$sql = "SELECT moldb_molstat.*, cdk_descriptors.* FROM sdftags left join inchi_key using (mol_id) left join cdk_descriptors using (mol_id) left join moldb_molstat using (mol_id) WHERE mol_id=:id or inchi_key.smiles=:id or inchi_key.inchi=:id or inchi_key.inchi_key=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$singledataset = $stmt->fetchObject(); # ->fetchAll(PDO::FETCH_OBJ);  
+		$db = null;
+		echo json_encode($singledataset); 
+		#echo '{"compounds": ' . json_encode($singledataset) . '}'; 
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function getCompoundModelFingerprint($id) {
+
+        $id = str_replace("_","/",$id);
+
+	$sql = "SELECT prediction_list.model_id, prediction_mols.lhood FROM prediction_mols left join prediction_list using (pred_id) WHERE mol_id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$singledataset = $stmt->fetchAll(PDO::FETCH_OBJ);
+		//print_r($singledataset); 
+		$returnModelArray = array();
+		// push model id into array 
+        $returnModelArray["mol_id"] = $id;          
+    	foreach($singledataset as $i => $i_value) {
+            $returnModelArray["model_".$i_value->model_id] = $i_value->lhood;
+        }		 
+		$db = null;
+		echo json_encode($returnModelArray); 
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+
 function getCompoundModels($id) {
 
         $id = str_replace("_","/",$id);
 
-	$sql = "SELECT prediction_mols.*, prediction_list.model_id, prediction_list.batch_id FROM prediction_mols left join inchi_key using (mol_id) join prediction_list using (pred_id) WHERE mol_id=:id or inchi_key.smiles=:id or inchi_key.inchi=:id or inchi_key.inchi_key=:id";
+	$sql = "SELECT prediction_mols.*, prediction_list.model_id, prediction_list.batch_id FROM prediction_mols left join prediction_list using (pred_id) WHERE mol_id=:id group by mol_id, model_id";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);  
@@ -116,7 +184,7 @@ function getCompoundModels($id) {
 }
 
 function getModels() {
-	$sql = "SELECT model_id, classes, data_type, class_tag, class_scheme  FROM class_models";
+	$sql = "SELECT model_id, name, classes, data_type, class_tag, class_scheme, info, pmid, filename FROM class_models left join batchlist using (batch_id)";
 	try {
 		$db = getConnection();
 		$stmt = $db->query($sql);  
